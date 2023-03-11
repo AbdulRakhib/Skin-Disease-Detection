b9707085e4a50773348c2792d9300c6dea44d576
Rakhib <abdul204.arf@gmail.com>
2023-03-11 22:22:39 +0530

    Updated Code

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+import os
+
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
+from keras_preprocessing.image import ImageDataGenerator
+from werkzeug.utils import secure_filename
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
@@ -114,8 +118,10 @@ def imageUpload():
 
     imageFile = request.files['image']
     img = Image.open(imageFile)
+    # save the image locally ########################################################################################################3
     # img = Image.open(img_file)
-    img_array = np.array(img)
+
+    #tf.keras.utils.save_img("uploadedImage/uploaded_image.jpg", img_array, data_format=None , file_format=None)
     #img_array = img_array / 255.0  # Normalize the image pixels
 
     # Resize the image to match the input shape of the model
@@ -125,25 +131,64 @@ def imageUpload():
 
     # Make a prediction using the loaded model
     #with open('C:/Users/abdul/Desktop/IIT/Lectures/Year 2/Data Science Project/UI/DERMOCURE/DERMOCURE/components/skinDetection.pkl', 'rb') as f:
-    f = open('C:/Users/abdul/Desktop/IIT/Lectures/Year 2/Data Science Project/UI/DERMOCURE/DERMOCURE/components/skinDetection.pkl', 'rb')
+    f = open('C:/Users/abdul/Desktop/IIT/Lectures/Year 2/Data Science Project/UI/DERMOCURE/DERMOCURE/components/ensembleModel.pkl', 'rb')
     loadedModel = pickle.load(f)
 
-    plt.imshow(img_array.astype("uint8"))
-    img_array = cv2.resize(img_array, dsize=(256, 256), interpolation=cv2.INTER_CUBIC)
-    img_array = tf.keras.preprocessing.image.img_to_array(img_array)
+    #img_array = np.array(img)
+    plt.imshow(img)
+
+    #tf.keras.preprocessing.image.DirectoryIterator(
+    #    "uploadedImage",
+    #    ImageDataGenerator,
+    #    target_size=(256, 256),
+    #    color_mode='rgb',
+    #    classes=None,
+    #    class_mode='binary',
+    #    batch_size=32,
+    #    shuffle=True,
+    #    seed=None,
+    #    data_format=None,
+    #    save_to_dir=None,
+    #    save_prefix='',
+    #    save_format='png',
+    #    follow_links=False,
+    #    subset=None,
+    #    interpolation='nearest',
+    #    keep_aspect_ratio=False,
+    #    dtype=None
+    #)
+    #test_datagen = ImageDataGenerator(rescale=1./255)
+    #test_data = test_datagen.flow_from_directory(
+    #    'uploadedImage',
+    #    target_size=(32, 32),
+    #    batch_size=8
+    #)
+
+    #img_array = tf.keras.preprocessing.image.img_to_array(img)
     #img_array = cv2.resize(img_array, dsize=(256, 256), interpolation=cv2.INTER_CUBIC)
-    img_array = tf.expand_dims(img_array, 0)
-    prediction = loadedModel.predict(img_array)
-    print(prediction)
+    #img_array = tf.image.resize(img_array, size=(256, 256))
+    #img_array = tf.expand_dims(img_array, 0)
+
+    image = img.convert('RGB')
+    image = image.resize((256, 256))
+    image = np.array(image, dtype=np.float32)
+    image /= 255.0
+    image = np.expand_dims(image, 0)
+    input_tensor = tf.convert_to_tensor(image)
+
+    prediction = loadedModel.predict(input_tensor)
     classNames = ['Acne', 'Eczema', 'Melanoma Skin Cancer Nevi and Moles', 'Psoriasis']
-    predicted_class = classNames[np.argmax(prediction)]
-    confidence = round(100 * (np.max(prediction)), 2)
+    predicted_class = classNames[np.argmax(prediction[0])]
+    confidence = round(100 * (np.max(prediction[0])))
+    print(prediction)
     plt.title(f"Predicted Disease: {predicted_class}.\n Accuracy level: {confidence}%")
     plt.axis("off")
     plt.show()
 
     f.close()
-    return render_template('dashboard.html')
+
+    user = User.query.filter_by(id=current_user.id).first()
+    return render_template('dashboard.html', username=user.username.upper())
 
 
 
